6bf5a72ef0fde542fc16d6b150401234d6ed4210
Kenneth Mano <kenneth95.km@gmail.com>
2023-03-13 20:32:18 +1300

    Adding error messages

--- solution.py
+++ solution.py
@@ -10,18 +10,18 @@ class AssurityApiTest(unittest.TestCase):
 
     def test_name_is_carbon_credits(self):
         """This test verifies that the name of the API is Carbon credits"""
-        self.assertEqual(self.response.json()["Name"], "Carbon credits")
+        self.assertEqual(self.response.json()["Name"], "Carbon credits", msg="API Name is not \"Carbon credits\".")
 
     def test_can_relist_is_true(self):
         """This test verifies that the CanRelist property of the API is true"""
-        self.assertTrue(self.response.json()["CanRelist"])
+        self.assertTrue(self.response.json()["CanRelist"], msg="CanRelist is not true.")
 
     def test_text_of_gallery_description(self):
         """This test verifies that the Promotions element with the name 'Gallery' has a Description that contains the text 'Good position in category'"""
         promotions = self.response.json()["Promotions"]
         for promotion in promotions:
             if promotion["Name"] == "Gallery":
-                self.assertIn(promotion["Description"], "Good position in category")
+                self.assertIn(promotion["Description"], "Good position in category", msg="Promotions element with the name 'Gallery' does not contain the text 'Good position in category'.")
 
 if __name__ == '__main__':
     unittest.main()
