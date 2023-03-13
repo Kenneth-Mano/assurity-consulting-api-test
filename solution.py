b044b8f41d9d2070353a11fc8f67b99fc9432d36
Kenneth Mano <kenneth95.km@gmail.com>
2023-03-13 20:41:49 +1300

    Increasing debugging capabilities with better error messages

--- solution.py
+++ solution.py
@@ -10,18 +10,21 @@ class AssurityApiTest(unittest.TestCase):
 
     def test_name_is_carbon_credits(self):
         """This test verifies that the name of the API is Carbon credits"""
-        self.assertEqual(self.response.json()["Name"], "Carbon credits", msg="API Name is not \"Carbon credits\".")
+        value = self.response.json()["Name"]
+        self.assertEqual(value, "Carbon credits", msg=f"API Name is {value}, which is not \"Carbon credits\".")
 
     def test_can_relist_is_true(self):
         """This test verifies that the CanRelist property of the API is true"""
-        self.assertTrue(self.response.json()["CanRelist"], msg="CanRelist is not true.")
+        value = self.response.json()["CanRelist"]
+        self.assertTrue(value, msg=f"CanRelist has the value {value}, which is not equal to true.")
 
     def test_text_of_gallery_description(self):
         """This test verifies that the Promotions element with the name 'Gallery' has a Description that contains the text 'Good position in category'"""
         promotions = self.response.json()["Promotions"]
         for promotion in promotions:
             if promotion["Name"] == "Gallery":
-                self.assertIn(promotion["Description"], "Good position in category", msg="Promotions element with the name 'Gallery' does not contain the text 'Good position in category'.")
+                value = promotion["Description"]
+                self.assertIn(value, "Good position in category", msg=f"The Promotions element with the name 'Gallery' has a 'Description' with the text {value}, which does not contain the text 'Good position in category'.")
 
 if __name__ == '__main__':
     unittest.main()
