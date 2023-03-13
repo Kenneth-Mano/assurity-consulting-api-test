b28836b5577fb00e5e58194fa4dbb476f79dbd08
Kenneth Mano <kenneth95.km@gmail.com>
2023-03-13 20:25:36 +1300

    Adding docstrings to tests

--- solution.py
+++ solution.py
@@ -4,16 +4,20 @@ import unittest
 class AssurityApiTest(unittest.TestCase):
     
     def setUp(self):
+        """This sets up the test by calling the API"""
         self.url = ' https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false'
         self.response = requests.get(self.url)
 
     def test_name_is_carbon_credits(self):
+        """This test verifies that the name of the API is Carbon credits"""
         self.assertEqual(self.response.json()["Name"], "Carbon credits")
 
     def test_can_relist_is_true(self):
+        """This test verifies that the CanRelist property of the API is true"""
         self.assertTrue(self.response.json()["CanRelist"])
 
     def test_text_of_gallery_description(self):
+        """This test verifies that the Promotions element with the name 'Gallery' has a Description that contains the text 'Good position in category'"""
         promotions = self.response.json()["Promotions"]
         for promotion in promotions:
             if promotion["Name"] == "Gallery":
